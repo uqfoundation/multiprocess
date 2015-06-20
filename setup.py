eb06f4a80c2e1097d317f0670e9e8565e4061f00
mmckerns <mmckerns@8bfda07e-5b16-0410-ab1d-fd04ec2748df>
2015-06-20 16:29:06 +0000

    removing README.txt; simpler long_description
    git-svn-id: svn+ssh://svn.mystic.cacr.caltech.edu/pathos/multiprocess@759 8bfda07e-5b16-0410-ab1d-fd04ec2748df

--- setup.py
+++ setup.py
@@ -191,18 +191,38 @@ else:
     if macros.get('HAVE_SEM_OPEN', False):
         multiprocessing_srcs.append('%s/semaphore.c' % srcdir)
 
-long_description = open(os.path.join(HERE, 'README.txt')).read()
-long_description += """
+long_description = '''
+`Multiprocessing` is a package for the Python language which supports the
+spawning of processes using the API of the standard library's
+`threading` module. `multiprocessing` has been distributed in the standard
+library since python 2.6.
 
-===========
-Changes
-===========
+Features:
 
-"""
-long_description += open(os.path.join(HERE, 'CHANGES.txt')).read()
-if not is_py3k:
-    long_description = long_description.encode('ascii', 'replace')
+* Objects can be transferred between processes using pipes or
+  multi-producer/multi-consumer queues.
 
+* Objects can be shared between processes using a server process or
+  (for simple data) shared memory.
+
+* Equivalents of all the synchronization primitives in `threading`
+  are available.
+
+* A `Pool` class makes it easy to submit tasks to a pool of worker
+  processes.
+
+'''
+#long_description = open(os.path.join(HERE, 'README.md')).read()
+#long_description += """
+#
+#===========
+#Changes
+#===========
+#
+#"""
+#long_description += open(os.path.join(HERE, 'CHANGES.txt')).read()
+#if not is_py3k:
+#    long_description = long_description.encode('ascii', 'replace')
 
 # -*- Installation Requires -*-
 py_version = sys.version_info

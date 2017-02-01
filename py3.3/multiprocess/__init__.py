6c22b44b7d82d15eca351ae38e6f35365c46488c
mmckerns <mmckerns@8bfda07e-5b16-0410-ab1d-fd04ec2748df>
2017-02-01 23:41:42 +0000

    nudging past multiprocess-0.70.5
    git-svn-id: svn+ssh://svn.mystic.cacr.caltech.edu/pathos/multiprocess@962 8bfda07e-5b16-0410-ab1d-fd04ec2748df

--- py3.3/multiprocess/__init__.py
+++ py3.3/multiprocess/__init__.py
@@ -16,7 +16,7 @@
 # Licensed to PSF under a Contributor Agreement.
 #
 
-__version__ = '0.70.5'
+__version__ = '0.70.6.dev0'
 
 __all__ = [
     'Process', 'current_process', 'active_children', 'freeze_support',

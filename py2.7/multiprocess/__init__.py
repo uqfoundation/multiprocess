7f36ed6f5d080aa6af002e2e14884c0c493c44f9
mmckerns <mmckerns@caltech.edu>
2020-06-15 20:39:43 -0400

    bumping to multiprocess-0.70.10

--- py2.7/multiprocess/__init__.py
+++ py2.7/multiprocess/__init__.py
@@ -41,7 +41,7 @@
 # SUCH DAMAGE.
 #
 
-__version__ = '0.70.10.dev0'
+__version__ = '0.70.10'
 
 __all__ = [
     'Process', 'current_process', 'active_children', 'freeze_support',
--- py3.9/multiprocess/__init__.py
+++ py3.9/multiprocess/__init__.py
@@ -15,7 +15,7 @@
 import sys
 from . import context
 
-__version__ = '0.70.10.dev0'
+__version__ = '0.70.10'
 
 #
 # Copy stuff from default context

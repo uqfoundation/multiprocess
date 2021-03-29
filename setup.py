ceb32b5dba62283ab75963b59b789245b84810f8
mmckerns <mmckerns@caltech.edu>
2021-03-29 12:57:07 -0400

    add support for py3.10

--- setup.py
+++ setup.py
@@ -414,6 +414,7 @@ def run_setup(with_extensions=True):
             'Programming Language :: Python :: 3.7',
             'Programming Language :: Python :: 3.8',
             'Programming Language :: Python :: 3.9',
+            'Programming Language :: Python :: 3.10',
             'Topic :: Scientific/Engineering',
             'Topic :: Software Development',
         ],
